f8dd90d0746789eefd6ee5325889faabfe546406
manish7739 <manishkumar12118@gmail.com>
2021-03-14 12:35:26 +0530

    change in auth 2

--- Swap.java
+++ Swap.java
@@ -4,12 +4,12 @@ class Swap
 	public static void main(String[] args)  {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter first Number");
-		int a=sc.nextInt();
-		System.out.println("Enter second Number");
-		int b=sc.nextInt();
-		int temp=a;
-		a=b;
-		b=temp;
+		// int a=sc.nextInt();
+		// System.out.println("Enter second Number");
+		// int b=sc.nextInt();
+		// int temp=a;
+		// a=b;
+		// b=temp;
 
 		System.out.println("first="+a);
 		System.out.println("second="+b);
