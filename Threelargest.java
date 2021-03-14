ca858073a0fe42a68a57b7eef3e6bc28a2b519bb
manish7739 <manishkumar12118@gmail.com>
2021-03-14 12:35:08 +0530

    change in auth 1

--- Threelargest.java
+++ Threelargest.java
@@ -2,18 +2,18 @@ class Threelargest
 {
 	public static void main(String[] args)
 	 {
-		int a=5,b=7,c=10;
-		if(a>=b && a>=c)
-		{
-            System.out.println("a is greater");
-		}
-		else if (b>=a && b>=c)
-		 {
-			System.out.println("b is greater");
-		}
-		else
-		{
-			System.out.println("c is greater");
-		}
+		// int a=5,b=7,c=10;
+		// if(a>=b && a>=c)
+		// {
+        //     System.out.println("a is greater");
+		// }
+		// else if (b>=a && b>=c)
+		//  {
+		// 	System.out.println("b is greater");
+		// }
+		// else
+		// {
+		// 	System.out.println("c is greater");
+		// }
 	}
 }
