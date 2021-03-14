42860df4f55430eb24996bf9486fa8a015999507
manish7739 <manishkumar12118@gmail.com>
2021-03-14 12:24:29 +0530

    even file change

--- Even.java
+++ Even.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class Even
 {
 	public static void main(String[] args) {
-	
+	int r=8;
 	Scanner s=new Scanner(System.in);
 	System.out.println("Enter a Number");
 	int a=s.nextInt();
